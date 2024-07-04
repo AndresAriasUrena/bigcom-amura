2b5131b5ecdb1835591f39ac547c0b1112bbb981
sheraz <sherazarshad419@gmail.com>
2024-07-04 18:19:58 +0500

    working on filters

--- app/categories/layout.tsx
+++ app/categories/layout.tsx
@@ -1,9 +1,5 @@
 import { Suspense } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="mx-8 w-[90%] max-w-[1200px] py-20 sm:mx-auto">
-      <Suspense>{children}</Suspense>
-    </div>
-  );
+  return <Suspense>{children}</Suspense>;
 }
--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,22 +1,16 @@
 import { getCategories } from '@/lib/bigcommerce';
-import Link from 'next/link';
+import Filters from './Filters';
 
 export default async function Page() {
   const Categories = await getCategories();
 
   return (
     <>
-      <div className="grid grid-cols-3 gap-4">
-        {Categories.map((category, index) => {
-          console.log(category);
-          return (
-            <div key={index}>
-              <Link href={`/categories${category.path}`} className="block h-full w-full">
-                {category.image ? <img src={category.image.urlOriginal} className="size-full object-cover" alt={category.image.urlOriginal} /> : <div className="flex min-h-[400px] items-center justify-center rounded-md border border-white/15 bg-black/75">{category.name}</div>}
-              </Link>
-            </div>
-          );
-        })}
+      <div className="bg-white">
+        <div className="mx-8 w-[90%] max-w-[1200px] py-16 sm:mx-auto">
+          {Categories.length === 0 && <p className="col-span-4 py-3 text-center text-2xl font-semibold text-black">No collections found</p>}
+          {Categories.length > 0 && <Filters categories={Categories} />}
+        </div>
       </div>
     </>
   );
