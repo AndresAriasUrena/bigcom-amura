1c48f72867cb05b3efb7c67ef54401a000d2f19d
sheraz <sherazarshad419@gmail.com>
2024-07-08 12:42:09 +0500

    updated category page

--- app/categories/[category]/Filters.tsx
+++ app/categories/[category]/Filters.tsx
@@ -81,25 +81,25 @@ export default function Filters({ items, category }: { items: categoryItems; cat
                 {/* Coleccionesa */}
                 <h4 className=" mt-0 text-[28px] font-light text-black">Colecciones</h4>
                 {filtersArr.map((item, index) => (
-                  <div className="flex cursor-pointer gap-3" key={index} onClick={() => loadFilteredProducts(item)}>
-                    <div className={`custom-checkbox group h-8  w-8 cursor-pointer border-4 border-black ${item.key === activeFilters && 'active'} ${item.key === category && activeFilters === '' && 'active'}`}>
-                      <svg xmlns="http://www.w3.org/2000/svg" className="hidden h-6 w-6 text-black group-[.active]:block" viewBox="0 0 24 24">
+                  <div className="flex cursor-pointer items-center gap-3" key={index} onClick={() => loadFilteredProducts(item)}>
+                    <div className={`custom-checkbox group size-6 cursor-pointer border-[3px] border-black ${item.key === activeFilters && 'active'} ${item.key === category && activeFilters === '' && 'active'}`}>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="hidden size-[18px] text-black group-[.active]:block" viewBox="0 0 24 24">
                         <path fill="currentColor" d="m9 20.42l-6.21-6.21l2.83-2.83L9 14.77l9.88-9.89l2.83 2.83z" />
                       </svg>
                     </div>
-                    <span className="text-2xl font-light text-black">{item.label}</span>
+                    <span className="text-xl font-light text-black">{item.label}</span>
                   </div>
                 ))}
                 {/* Notas */}
                 <h4 className="!mt-8 text-[28px] font-light text-black">Notas</h4>
                 {filtersArr2.map((item, index) => (
-                  <div className="flex cursor-pointer gap-3" key={index}>
-                    <div className={`custom-checkbox group h-8  w-8 cursor-pointer border-4 border-black ${item.key === activeFilters && 'active'} ${item.key === category && activeFilters === '' && 'active'}`}>
-                      <svg xmlns="http://www.w3.org/2000/svg" className="hidden h-6 w-6 text-black group-[.active]:block" viewBox="0 0 24 24">
+                  <div className="flex cursor-pointer items-center gap-3" key={index}>
+                    <div className={`custom-checkbox group size-6 cursor-pointer border-[3px] border-black ${item.key === activeFilters && 'active'} ${item.key === category && activeFilters === '' && 'active'}`}>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="hidden size-[18px] text-black group-[.active]:block" viewBox="0 0 24 24">
                         <path fill="currentColor" d="m9 20.42l-6.21-6.21l2.83-2.83L9 14.77l9.88-9.89l2.83 2.83z" />
                       </svg>
                     </div>
-                    <span className="text-2xl font-light text-black">{item.label}</span>
+                    <span className="text-xl font-light text-black">{item.label}</span>
                   </div>
                 ))}
               </div>
@@ -109,8 +109,8 @@ export default function Filters({ items, category }: { items: categoryItems; cat
             <div className={`grid gap-4 ${showFilters ? 'col-span-2 grid-cols-1 md:col-span-3 md:grid-cols-2 lg:col-span-3 xl:col-span-6 xl:grid-cols-3' : 'col-span-4 grid-cols-1 sm:grid-cols-2 md:col-span-5 md:grid-cols-3 xl:col-span-8'}`}>
               {products.products.map((item, index) => (
                 <div key={index}>
-                  <Link href={`/categories/${category}/${item.node.id}`} className="block h-full w-full rounded-md border">
-                    {item.node.images.edges[0].node.url ? <img src={item.node.images.edges[0].node.url} className="size-full rounded-md object-contain" alt={item.node.images.edges[0].node.altText} /> : <div className="flex h-full min-h-[175px] items-center justify-center rounded-md border border-white/15 bg-black sm:min-h-[275px] lg:min-h-[400px]">No Image Found</div>}
+                  <Link href={`/categories/${category}/${item.node.id}`} className="block h-full max-h-[358px] w-full rounded-md border">
+                    {item.node.images.edges[0].node.url ? <img src={item.node.images.edges[0].node.url} className="size-full rounded-md object-cover" alt={item.node.images.edges[0].node.altText} /> : <div className="flex h-full min-h-[175px] items-center justify-center rounded-md border border-white/15 bg-black sm:min-h-[275px] lg:min-h-[400px]">No Image Found</div>}
                   </Link>
                 </div>
               ))}
