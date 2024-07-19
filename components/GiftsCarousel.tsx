6bef31559bd5a04d9992f739681f2ff53012f716
Muhammad Fahad <mfd84739@gmail.com>
2024-07-19 15:39:27 +0500

    marquee slider

--- components/GiftsCarousel.tsx
+++ components/GiftsCarousel.tsx
@@ -12,7 +12,7 @@ export function GiftsCarousel({ gifts }: { gifts: giftProducts[] }) {
     <Marquee pauseOnHover={true}>
       {gifts.map((item, index) => (
         <Link href={'categories/ideas-para-regalar/' + item.node.id} key={index}>
-          <div className="group relative h-[600px] cursor-pointer overflow-hidden">
+          <div className="group relative h-[500px] cursor-pointer overflow-hidden 2xl:h-[600px]">
             <img src={item.node.images.edges[0].node.url} alt="gift image" className="mx-auto h-full w-full max-w-[90%] object-cover sm:max-w-[95%]" />
             <div className="absolute inset-0 top-[calc(100%+400px)] flex items-center justify-center duration-300 group-hover:top-0 group-hover:bg-black/50">
               <div className="h-fit max-w-[400px] px-5 text-center">
