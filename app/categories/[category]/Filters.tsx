c7ddaa41ff92a305a3e8283b67176743bb3133b1
sheraz <sherazarshad419@gmail.com>
2024-07-23 18:04:16 +0500

    filters done

--- app/categories/[category]/Filters.tsx
+++ app/categories/[category]/Filters.tsx
@@ -10,12 +10,6 @@ export default function Filters({ items, category }: { items: categoryItems; cat
   const [showFilters, setShowFilters] = useState(false);
   const [activeFilters, setActiveFilters] = useState('');
 
-  useEffect(() => {
-    if (window.innerWidth > 1200) {
-      setShowFilters(true);
-    }
-  }, [items]);
-
   const [filtersArr, setFiltersArr] = useState([
     { label: 'Para Él', key: 'para-el', active: false },
     { label: 'Para Ella', key: 'para-ella', active: false },
@@ -23,6 +17,16 @@ export default function Filters({ items, category }: { items: categoryItems; cat
     { label: 'Diseñador', key: 'dise-ador', active: false },
     { label: 'Alta Gama', key: 'alta-gama', active: false },
   ]);
+
+  useEffect(() => {
+    if (window.innerWidth > 1200) {
+      setShowFilters(true);
+    }
+    const updatedFilterscategory: any = filtersArr.map((item) => {
+      return item.key === category ? { ...item, active: true } : item;
+    });
+    setFiltersArr(updatedFilterscategory);
+  }, [items]);
 
   const [filtersArr2, setFiltersArr2] = useState([
     { label: 'frutales', key: 'frutales', active: false },
@@ -44,74 +48,91 @@ export default function Filters({ items, category }: { items: categoryItems; cat
     }
   };
 
-  const loadFilteredProducts = async (item: { label: string; key: string; active: boolean }) => {
-    setActiveFilters(item.key);
-
-    const res = await fetch('/api/hello', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ category: item.key }),
+  const loadFilteredProducts = async (item: { label: string; key: string; active: boolean }, index: number) => {
+    const updatedFilters = filtersArr.map((product: { label: string; key: string; active: boolean }, i) => {
+      if (i === index) {
+        return { ...product, active: !product.active };
+      }
+      return product;
     });
 
-    if (res.ok) {
-      const data = await res.json();
-      if (data.status === 'success') {
-        setProducts({ products: data.products.edges, totalItems: data.products.collectionInfo.totalItems });
-      } else {
-        alert(data.message);
+    setFiltersArr(updatedFilters);
+
+    let allProducts: any[] = [];
+    let totalItemsCount = 0;
+
+    for (const filter of updatedFilters) {
+      if (filter.active) {
+        try {
+          const res = await fetch('/api/get-products', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ category: filter.key }),
+          });
+
+          if (res.ok) {
+            const data = await res.json();
+            if (data.status === 'success') {
+              allProducts = [...allProducts, ...data.products.edges];
+              totalItemsCount += data.products.collectionInfo.totalItems;
+            } else {
+              alert(data.message);
+            }
+          }
+        } catch (error) {
+          console.error('Failed to fetch products:', error);
+        }
       }
     }
+
+    setProducts({ products: allProducts, totalItems: totalItemsCount });
   };
 
   return (
     <>
-      {/* show no collection found */}
-      {products.totalItems === 0 && <p className="col-span-4 py-3 text-center text-2xl font-semibold text-black">No collections found</p>}
       {/* show the products if the collection have products */}
-      {products.totalItems > 0 && (
-        <>
+      <>
+        {/* filters */}
+        <div className="mb-8 flex w-full justify-between text-black ">
+          <button onClick={toggleFilters} className="text-sm font-bold sm:text-lg">
+            Filtros <span className="text-sm">▼</span>
+          </button>
+          <p className="text-wrap text-sm font-semibold md:text-base">Ayudame a encontrar mi aroma ⓘ</p>
+        </div>
+
+        <div className="grid grid-cols-4 gap-4 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-8">
           {/* filters */}
-          <div className="mb-8 flex w-full justify-between text-black ">
-            <button onClick={toggleFilters} className="text-sm font-bold sm:text-lg">
-              Filtros <span className="text-sm">▼</span>
-            </button>
-            <p className="text-wrap text-sm font-semibold md:text-base">Ayudame a encontrar mi aroma ⓘ</p>
-          </div>
-
-          <div className="grid grid-cols-4 gap-4 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-8">
-            {/* filters */}
-            {showFilters && (
-              <div className="col-span-2 space-y-3">
-                {/* Coleccionesa */}
-                <h4 className=" mt-0 text-[28px] font-light text-black">Colecciones</h4>
-                {filtersArr.map((item, index) => (
-                  <div className="flex cursor-pointer items-center gap-3" key={index} onClick={() => loadFilteredProducts(item)}>
-                    <div className={`custom-checkbox group size-6 cursor-pointer border-[3px] border-black ${item.key === activeFilters && 'active'} ${item.key === category && activeFilters === '' && 'active'}`}>
-                      <svg xmlns="http://www.w3.org/2000/svg" className="hidden size-[18px] text-black group-[.active]:block" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="m9 20.42l-6.21-6.21l2.83-2.83L9 14.77l9.88-9.89l2.83 2.83z" />
-                      </svg>
-                    </div>
-                    <span className="text-xl font-light text-black">{item.label}</span>
+          {showFilters && (
+            <div className="sticky top-0 col-span-2 h-fit space-y-3">
+              {/* Coleccionesa */}
+              <h4 className=" mt-0 text-[28px] font-light text-black">Colecciones</h4>
+              {filtersArr.map((item, index) => (
+                <div className="flex cursor-pointer items-center gap-3" key={index} onClick={() => loadFilteredProducts(item, index)}>
+                  <div className={`custom-checkbox group size-6 cursor-pointer border-[3px] border-black ${item.active && 'active'} `}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="hidden size-[18px] text-black group-[.active]:block" viewBox="0 0 24 24">
+                      <path fill="currentColor" d="m9 20.42l-6.21-6.21l2.83-2.83L9 14.77l9.88-9.89l2.83 2.83z" />
+                    </svg>
                   </div>
-                ))}
-                {/* Notas */}
-                <h4 className="!mt-8 text-[28px] font-light text-black">Notas</h4>
-                {filtersArr2.map((item, index) => (
-                  <div className="flex cursor-pointer items-center gap-3" key={index}>
-                    <div className={`custom-checkbox group size-6 cursor-pointer border-[3px] border-black ${item.key === activeFilters && 'active'} ${item.key === category && activeFilters === '' && 'active'}`}>
-                      <svg xmlns="http://www.w3.org/2000/svg" className="hidden size-[18px] text-black group-[.active]:block" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="m9 20.42l-6.21-6.21l2.83-2.83L9 14.77l9.88-9.89l2.83 2.83z" />
-                      </svg>
-                    </div>
-                    <span className="text-xl font-light text-black">{item.label}</span>
+                  <span className="text-xl font-light text-black">{item.label}</span>
+                </div>
+              ))}
+              {/* Notas */}
+              <h4 className="!mt-8 text-[28px] font-light text-black">Notas</h4>
+              {filtersArr2.map((item, index) => (
+                <div className="flex cursor-pointer items-center gap-3" key={index}>
+                  <div className={`custom-checkbox group size-6 cursor-pointer border-[3px] border-black ${item.key === activeFilters && 'active'} ${item.key === category && activeFilters === '' && 'active'}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="hidden size-[18px] text-black group-[.active]:block" viewBox="0 0 24 24">
+                      <path fill="currentColor" d="m9 20.42l-6.21-6.21l2.83-2.83L9 14.77l9.88-9.89l2.83 2.83z" />
+                    </svg>
                   </div>
-                ))}
-              </div>
-            )}
+                  <span className="text-xl font-light text-black">{item.label}</span>
+                </div>
+              ))}
+            </div>
+          )}
 
-            {/* cards */}
+          {/* cards */}
+          {products.totalItems > 0 ? (
             <div className={`grid gap-4 ${showFilters ? 'col-span-2 grid-cols-1 md:col-span-3 md:grid-cols-2 lg:col-span-3 xl:col-span-6 xl:grid-cols-3' : 'col-span-4 grid-cols-1 sm:grid-cols-2 md:col-span-5 md:grid-cols-3 xl:col-span-8'}`}>
               {products.products.map((item, index) => (
                 <div key={index}>
@@ -121,9 +142,11 @@ export default function Filters({ items, category }: { items: categoryItems; cat
                 </div>
               ))}
             </div>
-          </div>
-        </>
-      )}
+          ) : (
+            <p className="col-span-2 py-3 text-center text-2xl font-semibold text-black xl:col-span-6">No Products found</p>
+          )}
+        </div>
+      </>
     </>
   );
 }
