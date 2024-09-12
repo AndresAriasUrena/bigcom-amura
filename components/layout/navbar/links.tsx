dabea0138513e655b09c828998b8a9b35c4ffa14
Andres Arias <andresarias@Andress-MacBook-Pro.local>
2024-09-12 11:30:41 -0600

    Update links.tsx to avoid Hooks related error

--- components/layout/navbar/links.tsx
+++ components/layout/navbar/links.tsx
@@ -5,7 +5,8 @@ import { VercelMenu as Menu } from '@/lib/bigcommerce/types';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
 
-export default function links({ menu }: { menu: Menu[] }) {
+// Cambiamos el nombre de 'links' a 'Links'
+export default function Links({ menu }: { menu: Menu[] }) {
   const params = useParams();
 
   return (
@@ -14,7 +15,10 @@ export default function links({ menu }: { menu: Menu[] }) {
         <ul className="flex w-full max-w-[800px] justify-between px-16 pb-4 lg:max-w-[1000px]">
           {menu.map((item) => (
             <li key={item.name}>
-              <Link href={'/categories' + item.path} className={`font-Julius_Sans_One text-base uppercase underline-offset-4 duration-200 hover:!text-c2 lg:text-lg ${params.product ? 'font-semibold text-black' : 'text-white'}`}>
+              <Link 
+                href={'/categories' + item.path} 
+                className={`font-Julius_Sans_One text-base uppercase underline-offset-4 duration-200 hover:!text-c2 lg:text-lg ${params.product ? 'font-semibold text-black' : 'text-white'}`}
+              >
                 {item.name}
               </Link>
             </li>
